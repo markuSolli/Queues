0bd2b308f3abf87e00f9eee22699422274161d27
Nicolay Roness <nicroness@gmail.com>
2022-03-28 15:49:20 +0200

    Implemented persitance support

---
+++ src/main/java/no/ntnu/fullstack/queues/user/User.java
@@ -0,0 +1,22 @@
+package no.ntnu.fullstack.queues.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
