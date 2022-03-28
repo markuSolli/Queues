11574713e81e7bdac5e2bf8d433331c15b742682
markussp <markussp@stud.ntnu.no>
2022-03-28 20:15:01 +0200

    Added Course

---
+++ src/main/java/no/ntnu/fullstack/queues/course/Course.java
@@ -0,0 +1,81 @@
+package no.ntnu.fullstack.queues.course;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Date;
+
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String code;
+    private String title;
+    private Date startDate;
+    private Date endDate;
+    private boolean arhived = false;
+    private boolean active = false;
+
+    protected Course(){}
+
+    public Course(String code, String title, Date startDate, Date endDate) {
+        this.code = code;
+        this.title = title;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public boolean isArhived() {
+        return arhived;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public void setArhived(boolean arhived) {
+        this.arhived = arhived;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
