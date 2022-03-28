0653627566f526e1cda0360a8c4d94eccbd9b1b0
markussp <markussp@stud.ntnu.no>
2022-03-28 15:26:52 +0200

    first commit

---
+++ src/main/java/no/ntnu/fullstack/queues/QueuesApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.fullstack.queues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueuesApplication.class, args);
+	}
+
+}
