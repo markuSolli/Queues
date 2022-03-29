5f562ab0a5e002499c6a8618d38415739a4e893b
Nicolay Roness <nicroness@gmail.com>
2022-03-29 11:41:59 +0200

    Setup course endpoints

--- src/main/java/no/ntnu/fullstack/queues/course/CourseController.java
+++ src/main/java/no/ntnu/fullstack/queues/course/CourseController.java
@@ -1,12 +1,10 @@
 package no.ntnu.fullstack.queues.course;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * The controller layer for Courses
@@ -14,6 +12,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/courses")
 public class CourseController {
+    private static final Logger logger = LoggerFactory.getLogger(CourseController.class);
+
     private final CourseService courseService;
 
     public CourseController(CourseService courseService) {
@@ -21,10 +21,36 @@ public class CourseController {
     }
 
     @GetMapping
-    public ResponseEntity<List<?>> getAllCourses() {
-        List<?> courses = courseService.getAllCourses();
+    public ResponseEntity<Iterable<Course>> getAllCourses() {
+        Iterable<Course> courses = courseService.getAll();
         return new ResponseEntity<>(courses, HttpStatus.OK);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> getCourse(@PathVariable Long id) {
+        try {
+            return new ResponseEntity<>(courseService.getCourse(id), HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Course> addCourse(@RequestBody Course course) {
+        Course addedCourse = courseService.addCourse(course);
+        return new ResponseEntity<>(addedCourse, HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Course> editCourse(@RequestBody Course course) {
+        return new ResponseEntity<>(courseService.updateCourse(course), HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<HttpStatus> deleteCourse(@RequestBody Course course) {
+        courseService.deleteCourse(course);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
--- src/main/java/no/ntnu/fullstack/queues/course/CourseService.java
+++ src/main/java/no/ntnu/fullstack/queues/course/CourseService.java
@@ -1,14 +1,70 @@
 package no.ntnu.fullstack.queues.course;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CourseService {
+    private final CourseRepository courseRepository;
 
-    public List<?> getAllCourses() {
-        return new ArrayList<>();
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    /**
+     * Returns a list of all courses
+     *
+     * @return list of all courses
+     */
+    public Iterable<Course> getAll() {
+        return courseRepository.findAll();
+    }
+
+    /**
+     * Returns the course with the given id
+     *
+     * @param id id of the course
+     * @return the course
+     */
+    public Course getCourse(Long id) {
+        return courseRepository.findById(id).orElseThrow(() -> new CourseNotFoundException(id));
+    }
+
+
+    /**
+     * Adds the given course to the database
+     *
+     * @return the course that was added
+     */
+    public Course addCourse(Course course) {
+        return courseRepository.save(course);
+    }
+
+    /**
+     * Updates a course with the given course data
+     *
+     * @param course course with new data
+     * @return edited course
+     */
+    public Course updateCourse(Course course) {
+        Course existingCourse = courseRepository.findById(course.getId()).orElse(new Course());
+        existingCourse.setId(course.getId());
+        existingCourse.setCode(course.getCode());
+        existingCourse.setTitle(course.getTitle());
+        existingCourse.setActive(course.isActive());
+        existingCourse.setArchived(course.isArchived());
+        existingCourse.setRooms(course.getRooms());
+        return courseRepository.save(existingCourse);
+    }
+
+    /**
+     * Deletes the given course
+     *
+     * @param course course to delete
+     */
+    public void deleteCourse(Course course) {
+        courseRepository.deleteById(course.getId());
     }
 }
