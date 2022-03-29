52bf0d1e7120c888d84831400c50ecf5e4ee7cc6
markussp <markussp@stud.ntnu.no>
2022-03-29 14:50:55 +0200

    Added endpoints to Queue

--- src/main/java/no/ntnu/fullstack/queues/queue/QueueRepository.java
+++ src/main/java/no/ntnu/fullstack/queues/queue/QueueRepository.java
@@ -1,11 +1,10 @@
 package no.ntnu.fullstack.queues.queue;
 
-import no.ntnu.fullstack.queues.course.Course;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QueueRepository extends CrudRepository<Queue, Long> {
 
-    Iterable<Queue> findAllByCourse(Course course);
+    Iterable<Queue> findAllByCourseId(Long courseID);
 }
