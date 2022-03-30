3856091196ec04d6d0a2018deb181ab5bbdeb294
Nicolay Caspersen Roness <nicolayr@stud.ntnu.no>
2022-03-30 12:19:03 +0200

    Continiued work on user, course and the roles they have

--- src/main/java/no/ntnu/fullstack/queues/user/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package no.ntnu.fullstack.queues.user;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
-
-public interface RoleRepository extends CrudRepository<Role, Long> {
-    Optional<Role> findByName(String name);
-}
