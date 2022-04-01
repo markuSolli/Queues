838bcb0e6dc1fdbfbb6ce7663b8bab244ad7ac95
markussp <markussp@stud.ntnu.no>
2022-04-01 15:14:24 +0200

    Added Building endpoints

---
+++ src/main/java/no/ntnu/fullstack/queues/location/BuildingService.java
@@ -0,0 +1,33 @@
+package no.ntnu.fullstack.queues.location;
+
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class BuildingService {
+    private final BuildingRepository buildingRepository;
+
+    public BuildingService(BuildingRepository buildingRepository) {
+        this.buildingRepository = buildingRepository;
+    }
+
+    /**
+     * Add a new Building to the database
+     * @param building the new Building
+     * @return the saved building
+     */
+    public Building addBuilding(Building building) {
+        return buildingRepository.save(building);
+    }
+
+    /**
+     * Delete a building from the database
+     * @param building the building to delete
+     * @throws NoSuchElementException
+     */
+    public void deleteBuilding(Building building) throws NoSuchElementException{
+        if(!buildingRepository.existsById(building.getId())) throw new NoSuchElementException();
+        buildingRepository.delete(building);
+    }
+}
