1c9760fe304761231c6d42ed498e50dca5493f2f
Leonard <leonaro@stud.ntnu.no>
2022-04-04 19:46:47 +0200

    update test

--- src/frontend/tests/unit/CourseCard.spec.js
+++ src/frontend/tests/unit/CourseCard.spec.js
@@ -1,11 +1,20 @@
 import { shallowMount } from '@vue/test-utils'
 import CourseCard from '@/components/CourseCard.vue'
+import { createStore } from 'vuex'
 
 
 describe('CourseCard.vue test', () => {
   it('renders title of coursecard', () => {
+    const store = createStore({
+      role: "ADMIN"
+    })
     // render the component
     const wrapper = shallowMount(CourseCard, {
+      global: {
+        provide: {
+          store: store
+        },
+      },
         propsData: {
           cardInQueue: true,
           course: {
@@ -16,5 +25,6 @@ describe('CourseCard.vue test', () => {
     })
 
     expect(wrapper.find('.queue-element-1').text()).toBe("CODE1 TITLE")
+    expect(wrapper.find('.queue-element-4').text()).toBe("Progress:")
   })
 })
