b39b46d7db413c0f117e4dbc6eb47e166a5ec974
markussp <markussp@stud.ntnu.no>
2022-04-04 19:23:02 +0200

    Added authentication to Queue-package

--- src/main/java/no/ntnu/fullstack/queues/course/UserCourseRepository.java
+++ src/main/java/no/ntnu/fullstack/queues/course/UserCourseRepository.java
@@ -1,7 +1,9 @@
 package no.ntnu.fullstack.queues.course;
 
+import no.ntnu.fullstack.queues.user.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserCourseRepository extends CrudRepository<UserCourse, Long> {
 
+    boolean existsByUserAndCourse(User user, Course course);
 }
