9be1789cdd40d57cd410916db5608649af1316d6
Leonard <leonaro@stud.ntnu.no>
2022-04-04 18:15:20 +0200

    tests

---
+++ src/frontend/tests/unit/Button.spec.js
@@ -0,0 +1,16 @@
+import { shallowMount } from '@vue/test-utils'
+import Button from "@/components/Button.vue"
+
+
+describe('Button.vue test', () => {
+  it('renders title of button', () => {
+    // render the component
+    const wrapper = shallowMount(Button, {
+        propsData: {
+            title: "buttonTitle"
+          }
+    })
+
+    expect(wrapper.find(".title").text()).toBe("buttonTitle")
+  })
+})
--- src/frontend/tests/unit/example.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import About from '@/views/About.vue'
-
-describe('About.vue', () => {
-  it('renders properly', () => {
-    const wrapper = shallowMount(About)
-    expect(wrapper.find('h1').exists())
-  })
-})
